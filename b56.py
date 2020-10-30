905b6ab7e0f7ba40ffc94eeb2c1f9a46ba35fcdf
DirWalk <cole@dt.localdomain>
2020-10-29 20:27:30 -0400

    Moved argparser to allow to clear up run import

--- b56.py
+++ b56.py
@@ -2,12 +2,6 @@ import random
 import string
 import argparse
 
-def args(): 
-    parser = argparse.ArgumentParser(description='Script to make an unambiguous string.')
-    parser.add_argument('-l', metavar='#', type=int, help='The length of the string to be generated.', dest='int_mode')
-    args = parser.parse_args()
-    return args
-
 def random_string(string_length):
     all_characters = string.ascii_letters + string.digits
     ignored_characters = '0OoIl1'
@@ -16,8 +10,11 @@ def random_string(string_length):
         output += random.choice([s for s in all_characters if s not in ignored_characters])
     print(output)
 
-def run():
-    random_string(args().int_mode)
+def run(args):
+    random_string(args.int_mode)
 
 if __name__ == "__main__": 
-    run()
+    parser = argparse.ArgumentParser(description='Script to make an unambiguous string.')
+    parser.add_argument('-l', metavar='#', type=int, help='The length of the string to be generated.', dest='int_mode')
+    args = parser.parse_args()
+    run(args)
